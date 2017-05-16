73cc06a510ec33c7fb5b265b948d0c29d8888734
Mohamed El Aazioui <mohamed.elaazioui@gmail.com>
2017-05-16 18:44:42 +0200

    Config setting set up

--- GenericTestAutomationFramework/Config/Settings.cs
+++ GenericTestAutomationFramework/Config/Settings.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static GenericTestAutomationFramework.Base.Browser;
 
 namespace GenericTestAutomationFramework.Config
 {
-    class Settings
+    public class Settings
     {
+        internal static string IsReporting;
+
+        public static string TestType { get; set; }
+        public static string AUT { get; set; }
+        public static string BuildName { get; set; }
+        public static BrowserType BrowserType { get; set; }
+
+        public static string IsLog { get; set; }
+        public static string LogPath { get; set; }
+
     }
 }
