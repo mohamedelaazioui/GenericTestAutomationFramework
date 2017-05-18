49a67441ac0a72bae4da983d7a8bc3384ff2d93e
Mohamed El Aazioui <mohamed.elaazioui@gmail.com>
2017-05-18 19:31:11 +0200

    Extension finished

--- GenericTestAutomationFramework/Extensions/WebDriverExtensions.cs
+++ GenericTestAutomationFramework/Extensions/WebDriverExtensions.cs
@@ -1,12 +1,55 @@
-﻿using System;
+﻿using GenericTestAutomationFramework.Base;
+using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GenericTestAutomationFramework.Extensions
 {
-    class WebDriverExtensions
+    public static class WebDriverExtensions
     {
+
+        public static void WaitForPageLoaded(this IWebDriver driver)
+        {
+            driver.WaitForCondition(dri =>
+            {
+                string state = dri.ExecuteJs("return document.readyState").ToString();
+                return state == "complete";
+             }, 20);
+        }
+
+        public static void WaitForCondition<T>(this T obj, Func<T, bool> condition, int timeOut)
+        {
+            Func<T, bool> execute =
+                (arg) =>
+                {
+                    try
+                    {
+                        return condition(arg);
+                    }
+                    catch (Exception e)
+                    {
+                        return false;
+                    }
+
+                };
+            var stopWatch = Stopwatch.StartNew();
+            while(stopWatch.ElapsedMilliseconds < timeOut )
+            {
+                if (execute(obj))
+                {
+                    break;
+                }
+            }
+
+        }
+
+        internal static object ExecuteJs(this IWebDriver driver, string script)
+        {
+            return ((IJavaScriptExecutor)DriverContext.Driver).ExecuteScript(script);
+        }
     }
 }
