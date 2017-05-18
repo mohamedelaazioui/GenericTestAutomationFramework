40c29d290491557c50c2d9132e82011bbfa48946
Mohamed El Aazioui <mohamed.elaazioui@gmail.com>
2017-05-18 20:07:38 +0200

    improvments added

--- GenericTestAutomationFramework/Base/TestInitializeHook.cs
+++ GenericTestAutomationFramework/Base/TestInitializeHook.cs
@@ -1,12 +1,66 @@
-﻿using System;
+﻿using EAAutoFramework.Config;
+using GenericTestAutomationFramework.Config;
+using GenericTestAutomationFramework.Helpers;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static GenericTestAutomationFramework.Base.Browser;
 
 namespace GenericTestAutomationFramework.Base
 {
-    class TestInitializeHook
+    public abstract class TestInitializeHook
     {
+        public readonly BrowserType Browser;
+
+        public TestInitializeHook(BrowserType browser)
+        {
+            Browser = browser;
+        }
+        public void InitializeSettings()
+        {
+            ConfigReader.SetFrameworkSettings();
+
+            // Set Log
+            LogHelpers.CreateLogFile();
+
+            // Open Browser
+            OpenBrowser(Browser);
+
+            LogHelpers.Write("Initialized Framework");
+        }
+
+        private void OpenBrowser(BrowserType browserType = BrowserType.Chrome)
+        {
+            switch (browserType)
+            {
+                case BrowserType.InternetExplorer:
+                    DriverContext.Driver = new InternetExplorerDriver();
+                    DriverContext.Browser = new Browser(DriverContext.Driver);
+                    break;
+
+                case BrowserType.Chrome:
+                    DriverContext.Driver = new ChromeDriver();
+                    DriverContext.Browser = new Browser(DriverContext.Driver);
+                    break;
+
+                case BrowserType.Firefox:
+                    DriverContext.Driver = new ChromeDriver();
+                    DriverContext.Browser = new Browser(DriverContext.Driver);
+                    break;
+            }
+        }
+
+        public virtual void NavigateSite()
+        {
+            DriverContext.Browser.GoToUrl(Settings.AUT);
+            LogHelpers.Write("Opened the browser !!!");
+        }
+       
+
+        
     }
 }
--- GenericTestAutomationFramework/Helpers/LogHelpers.cs
+++ GenericTestAutomationFramework/Helpers/LogHelpers.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GenericTestAutomationFramework.Config;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -16,7 +17,7 @@ namespace GenericTestAutomationFramework.Helpers
         // Create a file which can store the log information
         public static void CreateLogFile()
         {
-            string dir = @"c:\GenericFreameworkLog\";
+            string dir = Settings.LogPath;
             if(Directory.Exists(dir))
             {
                 _streamw = File.AppendText(dir + _logFileName + ".log");
